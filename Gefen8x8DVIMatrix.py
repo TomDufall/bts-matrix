53981210580c0a3515a29a48d6bbd5e9ee455084
Tom Dufall <td411@bath.ac.uk>
2018-12-31 03:31:17 +0000

    Implement serial and cleanup
    Fully implemented serial command sending for the core routing commands - ready for testing
    Cut out functions now handled by generic Matrix class

--- Gefen8x8DVIMatrix.py
+++ Gefen8x8DVIMatrix.py
@@ -1,61 +1,76 @@
 # Gefen 8x8 DVI Matrix controller over serial
 
 from Matrix import Matrix
+# requires pyserial
+import serial as pyserial
 
 class Gefen8x8DVIMatrix(Matrix):
 
-    def __init__(self):
-        # will probably need some sort of arg for the serial port
+    def __init__(self, serial_port):
         super().__init__(input_count = 8, output_count = 8)
+        self.serial = pyserial.Serial()
+        self.serial.port = serial_port
+        # Hard coded serial settings from the matrix manual
+        self.serial.baudrate = 19200
+        self.serial.bytesize = 8
+        self.serial.parity = 'N'
+        self.serial.stopbits = 1
+        self.serial.timeout = 2 # May need to tweak during testing
+        self.serial.open()
 
     def toLetter(self, number):
         "Convert a number to its corresponding letter in the alphabet (1-indexed). 1->A, 2->B, etc."
         return chr(number + 96).upper()
 
+    def send(self, msg):
+        "Send a string over serial"
+        self.serial.write(str.encode(msg))
+
+    def sendLine(self, msg):
+        "Send a string over serial with a carriage return"
+        self.send(msg + '\r')
+
     def patch(self, output, input):
         "Patch an input [1-n] to an output [1-n]"
-        print('Will send serial command {}'.format(self.toLetter(output) + str(input)))
-
-    def patchMulti(self, patch):
-        "A list of patch instructions, each given as a tuple {output, input}"
-        print('Not yet implemented in text')
-
-    def patchList(self, patch):
-        "An ordered list of inputs, to be applied to outputs in order. I.e. 1, 1, 2, 3 implies 1->1, 1->2, 2->2, 3->3"
-        print('Not yet implemented in text')
-
-    def patchAll(self, input):
-        "Patch the given input to every output"
-        print('Patching all outputs to input {}'.format(input))
-
-    def patchOneToOne(self):
-        "Patch 1->1, 2->2, etc. If more outputs than inputs, follow default hardware behaviour, or keep using the final input."
-        print('Patching all outputs one to one')
+        self.send('{}'.format(self.toLetter(output) + str(input)))
 
     def getRoutingTable(self):
         "Return the current routing table as a list of {output, input} tuples."
-        print('Not yet implemented in text')
+        # need to implement reading from serial - need to test with hardware to find format to parse
+        self.send('M')
 
     def blackout(self, output):
         "Black out the given output."
-        print('Will send serial command #MASK {}'.format(self.toLetter(output)))
+        self.sendLine('#MASK {}'.format(self.toLetter(output)))
 
     def unblackout(self, output):
         "Restore the given output from blackout."
-        print('Will send serial command #UNMASK {}'.format(self.toLetter(output)))
+        self.sendLine('#UNMASK {}'.format(self.toLetter(output)))
 
-    def blackoutMulti(self, inputs):
+    def blackoutMulti(self, outputs):
         "Blackout the given outputs."
-        print('Not yet implemented in text')
+        str = '#MASK'
+        for output in outputs:
+            str += ' {}'.format(self.toLetter(output))
+        self.sendLine(str)
 
-    def unblackoutMulti(self, inputs):
+    def unblackoutMulti(self, outputs):
         "Restore the given outputs from blackout."
-        print('Not yet implemented in text')
+        str = '#UNMASK'
+        for output in outputs:
+            str += ' {}'.format(self.toLetter(output))
+        self.sendLine(str)
 
     def blackoutAll(self):
         "Black out every output."
-        print('Blacking out all outputs')
+        str = '#MASK'
+        for output in range(1, self.OUTPUT_COUNT + 1):
+            str += ' {}'.format(self.toLetter(output))
+        self.sendLine(str)
 
     def unblackoutAll(self):
         "Restore every output from blackout."
-        print('Un-blacking out all outputs')
+        str = '#MASK'
+        for output in range(1, self.OUTPUT_COUNT + 1):
+            str += ' {}'.format(self.toLetter(output))
+        self.sendLine(str)
