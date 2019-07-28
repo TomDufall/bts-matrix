1ea0840991eb401ec0bc17d5ac023a86a39cc7cd
Tom Dufall <tom@tomdufall.co.uk>
2019-07-28 22:16:18 +0100

    Update tests * Update tests to reflect new method names and implementations

--- test_VirtualPresetController.py
+++ test_VirtualPresetController.py
@@ -1,231 +1,135 @@
-# Test virtual preset controller
+"""Test VirtualPresetController"""
 
 from VirtualPresetController import VirtualPresetController
 
-def test_save_single():
-    # Test saving a single preset when no presets and when other presets exist
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    vpc.save(preset1)
-    assert preset1 in vpc.presets
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    vpc.save(preset2)
-    assert preset2 in vpc.presets
+# Protected members of VirtualPresetController will be accessed for testing
+# pylint: disable=W0212
 
-def test_save_single_overwrite():
-    # Test saving a single preset to a presetNo that is already in use
+def test_delete_all_presets():
+    """Ensure all presets are deleted"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    vpc.save(preset1)
-    assert preset1 in vpc.presets
-    preset2 = ((1, [2, 2, 4, 2, 1, 1, 4, 7]))
-    vpc.save(preset2)
-    # Should have overwritten the existing preset
-    assert preset2 in vpc.presets
-    assert preset1 not in vpc.presets
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    vpc._presets = {'p1': p1, 'p2': p2}
+    vpc.delete_all_presets()
+    assert vpc._presets == {}
 
-def test_save_single_patchList_None():
-    # Test saving a patchList that is None
-    # Should fail silently
+def test_delete_preset():
+    """Delete a singe preset and verify others remain"""
     vpc = VirtualPresetController()
-    preset1 = ((1, None))
-    vpc.save(preset1)
-    assert vpc.presets == []
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    vpc._presets = {'p1': p1, 'p2': p2}
+    vpc.delete_preset('p1')
+    assert vpc._presets == {'p2': p2}
 
-def test_save_single_patchNo_not_int():
-    # Test saving non-int preset numbers
-    # Should fail silently
+def test_get_preset_exist():
+    """Test getting a preset that exists"""
     vpc = VirtualPresetController()
-    pl = [1, 1, 2, 2, 3, 3, 4, 5]
-    vpc.save((1.0, pl))
-    vpc.save((None, pl))
-    vpc.save(("Eric", pl))
-    assert vpc.presets == []
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    p3 = [2, 3, 1]
+    vpc._presets = {'p1': p1, 'p2': p2, 'p3': p3}
+    assert vpc.get_preset('p2') == p2
 
-def test_save_multi():
-    # Test saving multiple patches in one function
+def test_get_preset_not_exist():
+    """Test getting a preset that exists"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset3 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.save([preset1, preset2, preset3])
-    assert preset1 in vpc.presets
-    assert preset2 in vpc.presets
-    assert preset3 in vpc.presets
-    
-def test_move_single_no_conflict():
-    # Test moving a preset to an unused number
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    vpc.move((1, 3))
-    assert preset1 not in vpc.presets
-    assert (3, preset1[1]) in vpc.presets
-    assert preset2 in vpc.presets
-    assert preset4 in vpc.presets
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    p3 = [2, 3, 1]
+    vpc._presets = {'p1': p1, 'p2': p2, 'p3': p3}
+    assert vpc.get_preset('p4') is None
 
-def test_move_single_yes_conflict():
-    # Test moving a preset to a conflicting number
-    # Should overwrite
+def test_get_all_presets():
+    """Test getting all presets"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    vpc.move((1, 4))
-    assert preset1 not in vpc.presets
-    assert preset4 not in vpc.presets
-    assert (4, preset1[1]) in vpc.presets
-    assert preset2 in vpc.presets
-    
-def test_get_single():
-    # Test retrieving a single preset
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    assert preset1 is vpc.get(1)
-    assert preset2 is vpc.get(2)
-    assert preset4 is vpc.get(4)
-    assert preset1 in vpc.presets # make sure it's still there
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    p3 = [2, 3, 1]
+    vpc._presets = {'p1': p1, 'p2': p2, 'p3': p3}
+    response = vpc.get_all_presets()
+    assert ('p1', p1) in response
+    assert ('p2', p2) in response
+    assert ('p3', p3) in response
 
-def test_get_not_there():
-    # Test retrieving a single preset that doesn't exist
+def test_list_presets():
+    """Test listing all presets"""
     vpc = VirtualPresetController()
-    assert vpc.get(1) == (1, None)
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    assert vpc.get(3) == (3, None)
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    p3 = [2, 3, 1]
+    vpc._presets = {'p1': p1, 'p2': p2, 'p3': p3}
+    response = vpc.list_presets()
+    assert 'p1' in response
+    assert 'p2' in response
+    assert 'p3' in response
 
-def test_get_multi_all_there():
-    # Test retrieving multiple presets
+def test_move_preset():
+    """Test renaming a preset"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    result = vpc.get([1, 4])
-    assert preset1 in result
-    assert preset2 not in result
-    assert preset4 in result
-    assert preset1 in vpc.presets # make sure it's still there
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    p3 = [2, 3, 1]
+    vpc._presets = {'p1': p1, 'p2': p2, 'p3': p3}
+    vpc.move_preset('p1', 'p3')
+    assert 'p1' not in vpc._presets
+    assert 'p2' in vpc._presets
+    assert 'p3' in vpc._presets
+    assert vpc._presets['p3'] == p1
 
-def test_get_multi_inc_not_there():
-    # Test retrieving multiple presets including one that doesn't exist
+def test_preset_id_in_use_exists():
+    """Test checking if a preset id is in use with an id in use"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    result = vpc.get([1, 3, 4])
-    assert preset1 in result
-    assert preset2 not in result
-    assert (3, None) in result
-    assert preset4 in result
-    assert preset1 in vpc.presets # make sure it's still there
+    p1 = [1, 4, 4]
+    vpc._presets = {'p1': p1}
+    assert vpc.preset_id_in_use('p1')
 
-def test_getAll():
-    # Test retrieving all presets
+def test_preset_id_in_use_not_exists():
+    """Test checking if a preset id is in use with an id not in use"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    result = vpc.getAll()
-    assert preset1 in result
-    assert preset2 in result
-    assert preset4 in result
-    assert preset1 in vpc.presets # make sure it's still there
+    p1 = [1, 4, 4]
+    vpc._presets = {'p1': p1}
+    assert not vpc.preset_id_in_use('p2')
 
-def test_delete_single_exists():
-    # Test deleting a single preset that exists
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    vpc.delete(2)
-    assert preset1 in vpc.presets
-    assert preset2 not in vpc.presets
-    assert preset4 in vpc.presets
+def test_random_string():
+    """Test that a string of the specified length is generated"""
+    str_ = VirtualPresetController.random_string(length=10)
+    assert len(str_) == 10
+    assert isinstance(str_, str)
 
-def test_delete_single_not_exists():
-    # Test deleting a single preset that doesn't exist
+def test_save_preset():
+    """Test that the preset is saved and the assigned id returned"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    vpc.delete(3)
-    assert preset1 in vpc.presets
-    assert preset2 in vpc.presets
-    assert preset4 in vpc.presets
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    vpc._presets = {'p1': p1}
+    p2_id = vpc.save_preset(p2)
+    assert 'p1' in vpc._presets
+    assert p2_id in vpc._presets
+    assert vpc._presets[p2_id] == p2
 
-def test_delete_multiple_exists():
-    # Test deleting multiple presets that exist
+def test_set_preset():
+    """Test that the preset is set, including the correct id"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    vpc.delete([1, 4])
-    assert preset1 not in vpc.presets
-    assert preset2 in vpc.presets
-    assert preset4 not in vpc.presets
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    vpc._presets = {'p1': p1}
+    vpc.set_preset('p2', p2)
+    assert 'p1' in vpc._presets
+    assert 'p2' in vpc._presets
+    assert vpc._presets['p2'] == p2
 
-def test_delete_multiple_inc_not_exists():
-    # Test deleting multiple presets including one that doesn't exist
+def test_set_presets():
+    """Test that the presets are set, including the correct ids"""
     vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    vpc.delete([1, 3, 4])
-    assert preset1 not in vpc.presets
-    assert preset2 in vpc.presets
-    assert preset4 not in vpc.presets
-
-def test_deleteAll():
-    # Test deleting all presets
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    vpc.deleteAll()
-    assert vpc.presets == []
-
-def test_exists_single_does_exist():
-    # Test if a single preset that does exist exists
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    assert vpc.exists(2) is True
-    assert preset2 in vpc.presets
-
-def test_exists_single_does_not_exist():
-    # Test if a single preset that doesn't exist exists
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    assert vpc.exists(3) is False
-
-def test_exists_multiple():
-    # Test if multiple presets exist, including one that doesn't
-    vpc = VirtualPresetController()
-    preset1 = ((1, [1, 1, 2, 2, 3, 3, 4, 5]))
-    preset2 = ((2, [2, 2, 4, 2, 1, 1, 4, 7]))
-    preset4 = ((4, [1, 1, 4, 5, 6, 7, 1 ,1]))
-    vpc.presets = [preset1, preset2, preset4]
-    result = vpc.exists([5, 4, 3, 2, 1])
-    truths = list(map(lambda x: x[1], result))
-    assert truths == [False, True, False, True, True]
+    p1 = [1, 4, 4]
+    p2 = [2, 2, 2]
+    p3 = [2, 3, 1]
+    vpc._presets = {'p1': p1}
+    vpc.set_presets([('p2', p2), ('p3', p3)])
+    assert 'p1' in vpc._presets
+    assert 'p2' in vpc._presets
+    assert 'p3' in vpc._presets
+    assert vpc._presets['p2'] == p2
+    assert vpc._presets['p3'] == p3
